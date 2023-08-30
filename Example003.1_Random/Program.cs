28c0ebf74948828f61ce6b79e8d410927d613958
elena bobina <putnyk19@mail.ru>
2023-08-30 22:09:59 +0500

    Random Sum

---
+++ Example003.1_Random/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму двух случайных чисел
+int numberA = new Random().Next(1, 10); // от 1 до 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine(numberB);
+int sum = numberA + numberB;
+Console.WriteLine(sum);
+
